392430c3fabc6dd2b610a4a9eda1486c5dac48ad
rofleksey <evilmorty@yandex.ru>
2023-03-21 22:54:22 +0300

    Fix required

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://maksakov.com/cms",
+  // url: "http://localhost/cms",
   app: {
     keys: env.array('APP_KEYS'),
   },
