0968a8af60dba473ebdf7e56f941fdce3efa09d4
rofleksey <evilmorty@yandex.ru>
2025-02-12 07:26:59 +0300

    Update Dockerfile

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://maksakov.com/cms",
+  url: "https://cms.maksakov.com/",
   // url: "http://localhost/cms",
   app: {
     keys: env.array('APP_KEYS'),
--- upload.sh
+++
@@ -1,3 +0,0 @@
-set -e
-docker build . -t rofleksey/maksakov-cms:latest
-docker push rofleksey/maksakov-cms:latest
